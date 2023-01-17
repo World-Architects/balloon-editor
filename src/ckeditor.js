5bc65c5ac52c94893a2c299d0f2ae568f6ff73a3
Falk Romano <fr@world-architects.com>
2023-01-17 11:15:12 +0100

    updated ballon editor plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,13 +7,18 @@ import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
+import SpecialCharactersCurrency from '@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency.js';
+import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials.js';
+import SpecialCharactersLatin from '@ckeditor/ckeditor5-special-characters/src/specialcharacterslatin.js';
+import SpecialCharactersMathematical from '@ckeditor/ckeditor5-special-characters/src/specialcharactersmathematical.js';
+import SpecialCharactersText from '@ckeditor/ckeditor5-special-characters/src/specialcharacterstext.js';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
 import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
 import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
@@ -28,13 +33,18 @@ Editor.builtinPlugins = [
 	BlockQuote,
 	Bold,
 	Essentials,
-	Image,
 	Indent,
 	Italic,
 	Link,
 	List,
 	Paragraph,
+	PasteFromOffice,
 	SpecialCharacters,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
+	SpecialCharactersText,
 	Strikethrough,
 	Subscript,
 	Superscript,
@@ -47,14 +57,15 @@ Editor.defaultConfig = {
 	toolbar: {
 		items: [
 			'bold',
-			'strikethrough',
 			'italic',
+			'underline',
+			'link',
 			'bulletedList',
 			'numberedList',
+			'strikethrough',
+			'|',
 			'subscript',
 			'superscript',
-			'link',
-			'|',
 			'specialCharacters',
 			'|'
 		]
